065014b1e6b9afe194dbd02e4d7aade88edaf495
binhttran <binh.tran8228@gmail.com>
2024-12-31 13:58:08 -0800

    fixed local time

--- src/components/contacts.jsx
+++ src/components/contacts.jsx
@@ -128,38 +128,22 @@ const ClockContainer = styled.div`
 
 
 const Clock = () => {
-
-  const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString());
-
-
-
-  useEffect(() => {
-
-    const interval = setInterval(() => {
-
-      setCurrentTime(new Date().toLocaleTimeString());
-
-    }, 1000);
-
-
-
-    return () => clearInterval(interval);
-
-  }, []);
-
-
-
-  return (
-
-    <ClockContainer>
-
-      {currentTime}
-
-    </ClockContainer>
-
-  );
-
-};
+    const [currentTime, setCurrentTime] = useState(
+      new Date().toLocaleTimeString('en-US', { timeZone: 'America/New_York' })
+    );
+  
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setCurrentTime(
+          new Date().toLocaleTimeString('en-US', { timeZone: 'America/New_York' })
+        );
+      }, 1000);
+  
+      return () => clearInterval(interval);
+    }, []);
+  
+    return <ClockContainer>{currentTime}</ClockContainer>;
+  };
 
 const Footer = styled.div`
     display: flex;
